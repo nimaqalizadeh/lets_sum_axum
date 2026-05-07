170a4d7d094121d7c5ca9dbc614978fad374ebe6
nimaqalizadeh <nimaqalizadeh@users.noreply.github.com>
2026-05-07 03:26:01 +0000

    deploy: 4a2e97650517d472069adbfad566b09ce4c769de

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d035bc48.js',
+                path_to_root + 'searchindex-a194e805.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
